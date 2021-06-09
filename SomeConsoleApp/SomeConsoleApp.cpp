1bab3ae98943f987d752949a02f298fdcb9c1cdb
Tomek Jaworski <tjaworski@tjaworski.pl>
2021-06-09 07:35:02 +0200

    Puste projekty + konfiguracja gita

---
+++ SomeConsoleApp/SomeConsoleApp.cpp
@@ -0,0 +1,9 @@
+// SomeConsoleApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::wcout << L"Hello World!\n";
+}
